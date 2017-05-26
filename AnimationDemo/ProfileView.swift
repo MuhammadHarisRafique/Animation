f695293762fc13d4229a17e50aa194f836ae4e4b
anas <anas.ejaz1991@gmail.com>
2017-05-26 19:26:00 +0500

    show user profile by animatiing

---
+++ AnimationDemo/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  AnimationDemo
+//
+//  Created by Higher Visibility on 24/05/2017.
+//  Copyright © 2017 HigherVisibility. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+
+}
--- AnimationDemo/ViewController.swift
+++ AnimationDemo/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   var myview = UIView()
+   var sideViewPresent = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      
+       self.presentSideView()
+       self.myview.backgroundColor = UIColor.black
+       self.view.addSubview(self.myview)
+    
+        self.addTabGestureOnView()
+        self.addTabGestureOnSuperView()
+ 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func presentSideView(){
+        
+        let x = self.view.frame.size.width - 50
+        let viewWidth = self.view.frame.size.width - 55
+        let viewHeight = self.view.frame.size.height - 50
+        self.myview.frame = CGRect(x: x, y: 20, width: viewWidth, height: viewHeight)
+        let xibView = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)
+        let main = xibView?[0] as! UIView
+        main.frame.size = self.myview.frame.size
+        self.myview.addSubview(main)
+        
+    }
+    
+    private func addTabGestureOnSuperView(){
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.movesideView(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tapGesture)
+        
     }
 
+    func movesideView(_ sender: UITapGestureRecognizer){
+    
+        if self.sideViewPresent == true{
+            
+        self.sideViewPresent = false
+            UIView.animate(withDuration:1, animations: {
+            self.presentSideView()
+            },completion:nil)
+        
+        
+        }
+    
+    }
+    
+    private func addTabGestureOnView(){
+    
+       let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.processONTabGesture(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        self.myview.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    func processONTabGesture(_ sender: UITapGestureRecognizer){
+        
+        self.sideViewPresent = true
+        UIView.animate(withDuration: 0.8, animations: {
+            let w = self.view.frame.size.width - 100
+            self.myview.frame.origin.x -= w
+        }, completion: nil)
+    
+    
+    }
 
+    @IBAction func animate_pressed(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.8, animations: {
+          self.sideViewPresent = true
+            let w = self.view.frame.size.width - 100
+            self.myview.frame.origin.x -= w
+        }, completion: nil)
+        
+        
+    }
 }
 
